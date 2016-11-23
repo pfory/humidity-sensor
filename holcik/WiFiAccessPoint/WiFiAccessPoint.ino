be63ba7be1388dc68b9e238cfdcb22332f578b80
pfory <pfory@seznam.cz>
2016-11-23 23:04:06 +0100

    web

--- holcik/WiFiAccessPoint/WiFiAccessPoint.ino
+++ holcik/WiFiAccessPoint/WiFiAccessPoint.ino
@@ -1,58 +1,73 @@
 #include <ESP8266WiFi.h>
+#include <SI7021.h>
+
+SI7021 sensor;
+
+#define TEMPERATURE_DIVIDOR 100.0
+#define MILIVOLT_TO_VOLT 1000.0
+
+#define SDA 12 // D6 - GPI12 on ESP-201 module
+#define SCL 14 // D5 - GPI14 on ESP-201 module
+
+#include <ESP8266WebServer.h>
+ESP8266WebServer serverA(80);
+
 // #include <WiFiClient.h> 
-// #include <ESP8266WebServer.h>
 // #include <FS.h>
 
 #define MaxHeaderLength 1600
-byte mode=1;
+//byte mode=1; //flash
+//byte mode=2; //setup
+byte mode=3; //test
+//byte mode=4; //mereni
 
 #include "FS.h"
 
 /* Set these to your desired credentials. */
-const char *ssid = "ESPHum";
-const char *password = "hum007";
+const char *ssid;
+const char *password;
 
 String HttpHeader = String(MaxHeaderLength);
 String HttpHeaderBak = String(MaxHeaderLength);
 
+volatile float temperature;
+volatile float humidity;
+
+
 WiFiServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
 void handleRoot() {
-  // String contentType = "text/html";
-  // File file = SPIFFS.open("/default.htm", "r");
-  // size_t sent = server.streamFile(file, contentType);
-  // file.close();
-  // char temp[1500];
-  // snprintf ( temp, 1500, "<html><head></head><body><form method=get><input type='radio' name=r value='1'> One<br><input type='radio' name=r value='2' checked>\
-      // Two<br><input type='radio' name=r value='3'> Three<br><input type=submit value=submit></form></body></html>");
-  // // snprintf ( temp, 1500, "<!DOCTYPE html><html lang='en' xmlns='http://www.w3.org/1999/xhtml'><head><meta charset='utf-8' />\
-        // // // <title>Vlhkoměr</title></head><body><span style='font-weight:bold; font-size:20pt;'>Vlhkoměr</span><span style='font-style:italic;'> \
-        // // // Verze FW: 1.0.1</span><br/><br/><table><tr><td style='text-align:right;'>AP SSID:</td><td><input id='SSID' type='text' /></td></tr><tr>\
-        // // // <td style='text-align:right;'>AP password:</td><td><input id='passw' type='text' /></td></tr><tr><td style='text-align:right;'>IP:</td><td>\
-        // // // <input id='IP' type='text' maxlength='15' /></td></tr><tr><td style='text-align:right;'>Maska:</td><td><input id='mask' type='text' \
-        // // // maxlength='15' /></td></tr><tr><td style='text-align:right;'>Gateway:</td><td><input id='gateway' type='text' maxlength='15' />\
-        // // // </td></tr><tr><td style='text-align:right;'>Xively API key:</td><td><input id='APIkey' type='text' /></td></tr><tr><td style=\
-        // // // 'text-align:right;'>Xively feed ID:</td><td><input id='FeedID' type='text' /></td></tr><tr><td style='text-align:right;'>\
-        // // // Prodleva mezi měřeními [s]:</td><td><input id='delay' type='text' maxlength='5' /></td></tr><tr><td colspan='2' \
-        // // // style='text-align:center'><input id='Submit1' type='submit' value='Uložit' /></td></tr></table></body></html>");
-  // server.send ( 200, "text/html", temp );
-  
-  
-  Serial.println("HandleRoot");
-}
+	char temp[400];
 
-void handleClient() {
-  Serial.println("HandleClient");
+	snprintf ( temp, 400,
+
+"<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>Vlhkomer</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Vlhkomer</h1>\
+    <p>Teplota: %05d Vlhkost: %05d</p>\
+  </body>\
+</html>",
+
+		(int)(temperature/TEMPERATURE_DIVIDOR), (int)humidity
+	);
+	serverA.send ( 200, "text/html", temp );
 }
 
 void setup() {
-  if (mode==1) {
+  Serial.begin(115200);
+  if (mode==2) {
     delay(1000);
+    ssid = "ESPHum";
+    password = "hum007";
+
     //SPIFFS.begin();
-    Serial.begin(115200);
     Serial.println();
     Serial.print("Configuring access point...");
     /* You can remove the password parameter if you want the AP to be open. */
@@ -79,11 +94,40 @@ void setup() {
       HttpHeaderBak=f.readStringUntil('\n');
       Serial.println(HttpHeaderBak);
     }
+  } else if (mode==3) {
+    ssid = "Datlovo";
+    password = "Nu6kMABmseYwbCoJ7LyG";
+ 
+    IPAddress ip(192,168,1,166);  //Node static IP
+    IPAddress gateway(192,168,1,2);
+    IPAddress subnet(255,255,255,0);
+      
+    WiFi.mode(WIFI_STA);
+    WiFi.config(ip, gateway, subnet);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Gateway: ");
+    Serial.println(WiFi.gatewayIP());
+    Serial.print("Subnet: ");
+    Serial.println(WiFi.subnetMask());
+    Serial.print("MAC: ");
+    Serial.println(WiFi.macAddress());
+    sensor.begin(SDA,SCL);
+    serverA.on ( "/", handleRoot );
+    // server.on ( "/inline", []() {
+		// server.send ( 200, "text/plain", "this works as well" );
+      // } );
+    // server.onNotFound ( handleNotFound );
+  	serverA.begin();
+    Serial.println ( "HTTP server started" );
   }
 }
 
 void loop() {
-  if (mode==1) {
+  if (mode==2) {
     WiFiClient client = server.available();
       
     if (client) {
@@ -210,6 +254,21 @@ void loop() {
         }
       }
     }
+  } else if (mode==3) {
+    temperature=sensor.getCelsiusHundredths();
+    humidity=sensor.getHumidityPercent();
+    Serial.print("Temperature:");
+    Serial.print(temperature/TEMPERATURE_DIVIDOR);
+    Serial.println("C");
+    Serial.print("Humidity:");
+    Serial.print(humidity);
+    Serial.println("%");
+    float voltage = (float)ESP.getVcc()/MILIVOLT_TO_VOLT;
+    Serial.print("Voltage:");
+    Serial.print(voltage);
+    Serial.println("V");
+    serverA.handleClient();
+    delay(1000);
   }
 }
 
