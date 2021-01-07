2f5f29fbcb39010391d85759701116db461aab97
Petr <pfory@seznam.cz>
2021-01-07 23:20:28 +0100

    verze pro Arduino IDE a ESP201

---
+++ sklenik/Configuration.h
@@ -0,0 +1,95 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include "Sender.h"
+#include <DoubleResetDetector.h>      //https://github.com/khoih-prog/ESP_DoubleResetDetector
+#include <Ticker.h>
+#include "SI7021.h"
+
+
+//SW name & version
+#define     VERSION                      "0.01"
+#define     SW_NAME                      "VlhkomerSklenik"
+
+//#define timers
+#define verbose
+//#define time
+#ifdef time
+#include <TimeLib.h>
+#include <Timezone.h>
+#endif
+
+#define AUTOCONNECTPWD    "password"
+
+//#define ota
+#ifdef ota
+#include <ArduinoOTA.h>
+#define HOSTNAMEOTA       SW_NAME VERSION
+#define AUTOCONNECTNAME   HOSTNAMEOTA
+#else
+#define AUTOCONNECTNAME   SW_NAME VERSION
+#endif
+
+/*
+--------------------------------------------------------------------------------------------------------------------------
+
+Version history:
+
+--------------------------------------------------------------------------------------------------------------------------
+HW
+ESP8266 Wemos D1
+HC-SR04
+*/
+
+
+#define verbose
+#ifdef verbose
+  #define DEBUG_PRINT(x)         Serial.print (x)
+  #define DEBUG_PRINTDEC(x)      Serial.print (x, DEC)
+  #define DEBUG_PRINTLN(x)       Serial.println (x)
+  #define DEBUG_PRINTF(x, y)     Serial.printf (x, y)
+  #define DEBUG_PRINTHEX(x)      Serial.print (x, HEX)
+  #define PORTSPEED 115200
+  #define SERIAL_BEGIN           Serial.begin(PORTSPEED);
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTDEC(x)
+  #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRINTF(x, y)
+#endif 
+
+
+// Number of seconds after reset during which a
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 2
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
+
+#define CONFIG_PORTAL_TIMEOUT 60 //jak dlouho zustane v rezimu AP nez se cip resetuje
+#define CONNECT_TIMEOUT 120 //jak dlouho se ceka na spojeni nez se aktivuje config portal
+
+static const char* const      mqtt_server                    = "192.168.1.56";
+static const uint16_t         mqtt_port                      = 1883;
+static const char* const      mqtt_username                  = "datel";
+static const char* const      mqtt_key                       = "hanka12";
+static const char* const      mqtt_base                      = "/home/sklenik/esp10";
+
+
+int                           voltage                        = 0; //voltage in mV
+uint32_t                      lastRun                        = 0;
+int                           temperature                    = 0;
+uint8_t                       humidity                       = 0;
+uint32_t                      deepSleepMicros                = 0;
+
+
+#define TEMPERATURE_DIVIDOR                 100
+
+
+#define SCL                                 14 //                       GPIO14
+#define SDA                                 12 //                       GPIO12
+//#define BUILTIN_LED                         1
+
+
+#endif
---
+++ sklenik/sklenik.ino
@@ -0,0 +1,186 @@
+/*
+--------------------------------------------------------------------------------------------------------------------------
+Vlhkomer a teplomer sklenik
+Petr Fory pfory@seznam.cz
+GIT - https://github.com/pfory/
+*/
+
+#include "Configuration.h"
+
+//for LED status
+Ticker ticker;
+
+void tick() {
+  //toggle state
+  //int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
+  //digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  DEBUG_PRINTLN("Entered config mode");
+  DEBUG_PRINTLN(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  DEBUG_PRINTLN(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
+
+ADC_MODE(ADC_VCC); //vcc read
+
+bool isDebugEnabled() {
+#ifdef verbose
+  return true;
+#endif // verbose
+  return false;
+}
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+WiFiManager wifiManager;
+
+SI7021 sensor;
+
+/////////////////////////////////////////////   S  E  T  U  P   ////////////////////////////////////
+void setup(void) {
+  lastRun = millis();
+  SERIAL_BEGIN;
+  DEBUG_PRINT(F(SW_NAME));
+  DEBUG_PRINT(F(" "));
+  DEBUG_PRINTLN(F(VERSION));
+
+  //pinMode(BUILTIN_LED, OUTPUT);
+  ticker.attach(1, tick);
+
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.setConfigPortalTimeout(CONFIG_PORTAL_TIMEOUT);
+  wifiManager.setConnectTimeout(CONNECT_TIMEOUT);
+
+  if (drd.detectDoubleReset()) {
+    DEBUG_PRINTLN("Double reset detected, starting config portal...");
+    ticker.attach(0.2, tick);
+    if (!wifiManager.startConfigPortal(AUTOCONNECTNAME)) {
+      DEBUG_PRINTLN("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.reset();
+      delay(5000);
+    }
+  }
+
+  rst_info *_reset_info = ESP.getResetInfoPtr();
+  uint8_t _reset_reason = _reset_info->reason;
+  DEBUG_PRINT("Boot-Mode: ");
+  DEBUG_PRINTLN(_reset_reason);
+  
+  /*
+ REASON_DEFAULT_RST             = 0      normal startup by power on 
+ REASON_WDT_RST                 = 1      hardware watch dog reset 
+ REASON_EXCEPTION_RST           = 2      exception reset, GPIO status won't change 
+ REASON_SOFT_WDT_RST            = 3      software watch dog reset, GPIO status won't change 
+ REASON_SOFT_RESTART            = 4      software restart ,system_restart , GPIO status won't change 
+ REASON_DEEP_SLEEP_AWAKE        = 5      wake up from deep-sleep 
+ REASON_EXT_SYS_RST             = 6      external system reset 
+  */
+  client.setServer(mqtt_server, mqtt_port);
+  //client.setCallback(callback);
+
+  WiFi.printDiag(Serial);
+
+  if (!wifiManager.autoConnect(AUTOCONNECTNAME, AUTOCONNECTPWD)) { 
+    DEBUG_PRINTLN("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.deepSleep(3600e6);
+  } 
+
+#ifdef ota
+  ArduinoOTA.setHostname(HOSTNAMEOTA);
+
+  ArduinoOTA.onStart([]() {
+    DEBUG_PRINTLN("Start updating ");
+  });
+  ArduinoOTA.onEnd([]() {
+   DEBUG_PRINTLN("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    DEBUG_PRINTF("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    DEBUG_PRINTF("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+#endif
+
+  DEBUG_PRINT("SI7021 init..");
+  sensor.begin(SDA,SCL);
+  DEBUG_PRINTLN(".done");
+
+  ticker.detach();
+  //keep LED on
+  //digitalWrite(BUILTIN_LED, HIGH);
+
+  drd.stop();
+
+  DEBUG_PRINTLN(F("Setup end."));
+}
+ 
+/////////////////////////////////////////////   L  O  O  P   ///////////////////////////////////////
+void loop(void) {
+  //meassurement
+  DEBUG_PRINT("Temperature & humidity meas..");
+  temperature=sensor.getCelsiusHundredths();
+  humidity=sensor.getHumidityPercent();
+  DEBUG_PRINTLN(".done");
+  voltage = ESP.getVcc(); //in mV
+  //vypocet nasledujiciho deepsleep intervalu na zaklade napeti baterie
+  DEBUG_PRINT("Voltage:");
+  DEBUG_PRINTLN(voltage);
+  if (voltage > 4000) {
+    deepSleepMicros = 600e6; //10min
+  } else if (voltage > 3300) {
+    deepSleepMicros = 3600e6; //1hour
+  }
+  DEBUG_PRINT("Deep sleep:");
+  DEBUG_PRINT(deepSleepMicros/1e6);
+  DEBUG_PRINTLN(" sec.");
+
+  sendDataMQTT();
+  DEBUG_PRINT("Sleep for ");
+  DEBUG_PRINT(deepSleepMicros/1e6);
+  DEBUG_PRINTLN(" sec.");
+  ESP.deepSleep(deepSleepMicros);
+}
+
+void sendDataMQTT(void) {
+  //digitalWrite(BUILTIN_LED, LOW);
+  //printSystemTime();
+  DEBUG_PRINTLN(F("Data"));
+
+  SenderClass sender;
+  sender.add("VersionSW",         VERSION);
+  sender.add("voltage",           voltage);
+  sender.add("IP",                WiFi.localIP().toString().c_str());
+  sender.add("MAC",               WiFi.macAddress());
+  sender.add("temperature",       temperature/TEMPERATURE_DIVIDOR);  
+  sender.add("humidity",          humidity);
+  sender.add("bootTime",          (uint8_t)(millis() - lastRun));
+  sender.add("deepSleep",         (uint16_t)(deepSleepMicros/1e6));
+  
+  
+  DEBUG_PRINTLN(F("Calling MQTT"));
+  
+  sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base);
+  //digitalWrite(BUILTIN_LED, HIGH);
+  return;
+}
